52ca8a90f4bafa04e16bd41e6b9d1d701e571d2a
Rajan-rkg9 <amirajangupta@gmail.com>
2020-10-28 14:59:44 +0530

    [Rajan-rkg9] Added. Payroll data inserted into table

---
+++ VALUES INSERT.sql
@@ -0,0 +1,5 @@
+insert into employee_payroll (name,salary,start) values
+													   ('Bill' , 1000000.00 , '2018-01-03'),
+                                                       ('Diana' , 2000000.00 , '2018-11-13'),
+                                                       ('Charlie' , 3000000.00 , '2020-05-21');
+  select * from employee_payroll;
