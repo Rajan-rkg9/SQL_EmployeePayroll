433f28be8953b3c540f1d71a388c960ab448c1ac
Rajan-rkg9 <amirajangupta@gmail.com>
2020-10-29 23:38:21 +0530

    [Rajan-rkg9] Add. New records of same person added

---
+++ ADD NEW RECORD OF SAME PERSON.sql
@@ -0,0 +1,3 @@
+update employee_payroll set department = 'Sales' where name = 'Diana';
+insert into employee_payroll (name,department,Basic_Pay,Income_Tax,Deductions,Net_Pay,gender,start) values
+('Diana','Marketing',3000000,500000,100000,1500000,'F','2018-11-13');
